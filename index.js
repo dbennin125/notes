b3ca6143957174cb9cf551905e28b6c2918b7634
Daniel Bennington <dbennington125@gmail.com>
2020-06-01 17:12:12 -0700

    add test passed!

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ const minimist = require("minimist");
 
 console.log (minimist(process.argv));
 //get process.argvn
+
+// use parse to create an action from process.argv
+// use valid to check if it is a valid action
+// if valid execute
+// bring it all together
