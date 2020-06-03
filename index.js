8c3dd030de8da36a9f05fbd778829f3f929d6cb1
Daniel Bennington <dbennington125@gmail.com>
2020-06-02 17:29:30 -0700

    almost done

--- index.js
+++ index.js
@@ -1,24 +1,38 @@
 
-const { parse, validResponse } = require('./lib/input.js')
-const { execute } = require('./lib/notes.js')
+// const { parse, validResponse } = require('./lib/input.js')
+// const { execute } = require('./lib/notes.js')
+const Input = require('./Input');
+const Note  = require('./Notes');
+
+let input = new Input(process.argv);
+const isValidInput = input.isValid();
+
+isValidInput ? Notes.execute(input) : console.log('failure to add');
 
 
+
+
+
+
+
+//I will delete all this mess later, I'm just tring to get it working.
 //get process.argvn
-const userInput = parse(process.argv)
+// const userInput = parse(process.argv)
 // use parse to create an action from process.argv
 
 // const validUserInput = (validResponse(userInput));
-if(validResponse(userInput)) {
-     execute(userInput)
-} else {
-    return {
+// if(validResponse(userInput)) {
+//      execute(userInput)
+// } else {
+//     return {
         // error: console.error(userInput),
-           error: console.error(userInput, 'incorrect syntax, note not added')
-    };
-};
-console.log(userInput)
+//            error: console.error(userInput, 'incorrect syntax, note not added')
+//     };
+// };
+// console.log(userInput)
 // console.error(userInput)
 // use valid to check if it is a valid action
 // if valid execute
 // bring it all together
 //it works!!!!
+
