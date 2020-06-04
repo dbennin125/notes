012f0e1febe5c7c25dfd832878842774e66e4a49
Daniel Bennington <dbennington125@gmail.com>
2020-06-04 10:04:20 -0700

    added comments

--- index.js
+++ index.js
@@ -1,19 +1,39 @@
+const mongoose = require('mongoose');
+const Input = require('./Input');
+const Note = require('./lib/models/Note');
+
+mongoose.connect('mongodb://localhost:27017/notes', {
+      useNewUrlParser: true,
+      useUnifiedTopology :true 
+});
+
+const input = new Input(process.argv);
+
+if(input.isValid()) {
+        Note.execute(input)
+        .then(() => mongoose.connection.close());
+} else {
+        console.log('Opps, something went wrong');
+}
+
+
+
 
 // const { parse, validResponse } = require('./lib/input.js')
 // const { execute } = require('./lib/notes.js')
-const Input = require('./Input');
-const Note  = require('./Notes');
+// const Input = require('./Input');
+// const Note  = require('./Notes');
 
 
 
-let input = new Input(process.argv);
-const isValidInput = input.isValid();
+// let input = new Input(process.argv);
+// const isValidInput = input.isValid();
 
-isValidInput ? Note.execute(input) : console.log('failure to add');
+// isValidInput ? Note.execute(input) : console.log('failure to add');
 
 
-//I will delete all this mess later, I'm just tring to get it working.
-//get process.argvn
+//I will delete all this mess later, I'm just trying to get it working.
+//get process.argv
 // const userInput = parse(process.argv)
 // use parse to create an action from process.argv
 
